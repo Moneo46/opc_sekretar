03cb1f6c1ac57582fb4dec16f185eca9767cfd0c
Moneo <moneo.artem@gmail.com>
2020-05-28 17:35:33 +0300

    new commit

--- database/views.py
+++ database/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Registry
+from django.utils import timezone
 
 def create_record(request):
-	return render(request, 'database/create_record.html', {})
+	rows = Registry.objects.filter(date_created__lte=timezone.now()).order_by('date_created')
+	return render(request, 'database/create_record.html', {'rows':rows})
